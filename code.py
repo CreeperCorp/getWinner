f58b8d91b826854a5d28559743ea8e1289f2a708
CreepSplotion <dekaiyondavis8@gmail.com>
2025-11-13 21:50:17 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,11 +1,14 @@
 votesDictionary = []
 candidateVotes = {}
+ifContinue = 'yes'
+while ifContinue == 'yes':
+  votesDictionary.append(input("What word do you want to add to your list?: "))
+  ifContinue = input("Do you want to add anymore words to the list?(yes/no): ")
 for key in votesDictionary:
     candidateVotes[key] = 0
 def getWinner(x):
   for vote in votesDictionary:
       candidateVotes[vote] += 1
-      print(candidateVotes)
   return candidateVotes
 
 print(getWinner(votesDictionary))
